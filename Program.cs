ce53a920d683be7269c7606b5cb4b2adb3a7f62d
SandraEriksson <SandraErikss0n@hotmail.com>
2024-01-24 13:11:00 +0100

    Created function to save message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using chat_app_shared_c_;
 using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Net;
 using System.Net.Sockets;
@@ -120,8 +121,14 @@ namespace chat_server_c
                 var database = dbClient.GetDatabase("Users");
                 var collection = database.GetCollection<User>("users");
 
+                var maxUserId = collection.AsQueryable()
+            .OrderByDescending(u => u.UserId)
+            .Select(u => u.UserId)
+            .FirstOrDefault();
+
                 var user = new User
                 {
+                    UserId = maxUserId + 1,
                     Username = data[0],
                     Password = data[1],
                     Message = new List<string>()
@@ -132,6 +139,7 @@ namespace chat_server_c
                 reply = ($"User {data[0]} registered.");
                 byte[] replyBuffer = Encoding.ASCII.GetBytes(reply);
                 stream.Write(replyBuffer, 0, replyBuffer.Length);
+
             }
             else
             {
@@ -191,71 +199,21 @@ namespace chat_server_c
             string[] data = messageData.Split(',');
             string username = data[0];
             string message = data[1];
-            var user = collection.Find(u => u.Username == username);
+
+            var filter = Builders<User>.Filter.Eq(u => u.Username, username);
+            var user = collection.Find(filter).FirstOrDefault();
             if (user != null)
             {
-
+                var update = Builders<User>.Update.Push(u => u.Message, message);
+                collection.UpdateOne(filter, update);
+                Console.WriteLine("Message saved successfully.");
             }
+            else { Console.WriteLine("User not found."); }
 
 
         }
+
+
     }
 
 }
-
-/*const string clientString = "mongodb://localhost:27017";//Connection string
-MongoClient dbClient = new MongoClient(clientString); //Creating a MongoDB client using the clientString to localhost
-var database = dbClient.GetDatabase("Users"); //Access the database Users in MongoDB
-var collection = database.GetCollection<User>("users");//Reference the users collection on the database
-
-
-//Declaring variable username to store the entered username
-string username;
-
-//Using a do-while loop to reapeatedly prompt the user until a valid username is entered
-do
-{
-    Console.WriteLine("Enter username: "); //Asking the user to enter username using the terminal
-    username = Console.ReadLine()!; //Read the input for username from the terminal
-
-    if (string.IsNullOrWhiteSpace(username)) //Checking if the username string is null or whitespace
-    {
-        Console.WriteLine("Error: You need to enter a username."); //Error message for empty input
-    }
-} while (string.IsNullOrWhiteSpace(username)); //Condition for the loop to continue to execute as long as the username is null or a empty string
-
-
-//Declaring variable password to store the entered password
-string password;
-
-//Using a do-while loop to reapeatedly prompt the user until a valid password is entered
-do
-{
-    Console.WriteLine("Enter password: "); //Asking the user to enter password using the terminal
-    password = Console.ReadLine()!; //Read the input for password from the terminal
-
-    if (string.IsNullOrWhiteSpace(password)) //Checking if the password string is null or whitespace
-    {
-        Console.WriteLine("Error: You need to enter a password."); //Error message for empty input
-    }
-} while (string.IsNullOrWhiteSpace(password)); //Condition for the loop to continue to execute as long as the password is null or whitespaced
-
-if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password)) //Checks if either the username or password is null or whitespaced.
-{
-    Console.WriteLine("Error: You need to enter a password and/or username."); //Error message if the If statement is true.
-}
-else
-
-{
-    //Creating a new User using the entered username and password
-    var user = new User
-    {
-        Username = username,
-        Password = password
-    };
-
-    //Inserts the user to the MongoDB collection 
-    collection.InsertOne(user);
-
-    Console.WriteLine("User is now registered!");
-}*/
--- User.cs
+++ User.cs
@@ -1,7 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
 namespace chat_app_shared_c_
 {
     public class User
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public int UserId { get; set; }
+
         public string? Username { get; set; }
         public string? Password { get; set; }
         public List<string> Message { get; set; } = new List<string>();
