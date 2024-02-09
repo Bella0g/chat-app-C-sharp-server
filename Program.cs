0be2e76ee6a8a711cd3f26ee66b73e31c0f8daaf
Marcus Siming <siming.marcus@gmail.com>
2024-02-09 11:31:24 +0100

    Made some minor adjustments

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using chat_app_shared_c_;
+using Users;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Bson;
 using MongoDB.Driver;
@@ -16,15 +16,12 @@ namespace chat_server_c;
 
 class Server
 {
-    //TcpListener för att hantera inkommande anslutningar.
-    static TcpListener? tcpListener;
+    private static TcpListener? tcpListener;
     private static Dictionary<TcpClient, string> connectedUsers = new Dictionary<TcpClient, string>();
     private static IMongoCollection<User> collection;
 
     static void Main(string[] args)
     {
-        //Anger att tcpListener ska lyssna efter alla nätverksgränssnitt på port 27500.
-
         tcpListener = new TcpListener(IPAddress.Any, 27500);
         tcpListener.Start();
         Console.WriteLine("Server is listening on port 27500");
@@ -44,8 +41,7 @@ class Server
         }
     }
 
-    //MongoDB anslutning.
-    static void Mongodb()
+    private static void Mongodb()
     {
         const string databaseString = "mongodb://localhost:27017";
         MongoClient dbClient = new MongoClient(databaseString);
@@ -53,43 +49,37 @@ class Server
         collection = database.GetCollection<User>("users");
     }
 
-    //Hanterar enskilda klientanslutningar.
-    static void HandleClient(TcpClient client, NetworkStream stream)
+    private static void HandleClient(TcpClient client, NetworkStream stream)
     {
-        //Hämtar nätverksströmmen från klienten för dataöverföring.
-        //NetworkStream stream = client.GetStream();
         byte[] buffer = new byte[1024];
 
         try
         {
             int bytesRead;
 
-            //Läser in data från klienten så länge det finns data att läsa.
             while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
             {
-                //Konverterar den inkommande byte-arrayn till en sträng.
                 string userData = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-
                 string[] dataParts = userData.Split('.');
-                string dataType = dataParts[0];
+                string messageType = dataParts[0];
 
-                if (dataType == "REGISTER")
+                if (messageType == "REGISTER")
                 {
                     Register(dataParts[1], stream);
                 }
-                else if (dataType == "LOGIN")
+                else if (messageType == "LOGIN")
                 {
                     LogIn(dataParts[1], stream, client);
                 }
-                else if (dataType == "PUBLIC_MESSAGE")
+                else if (messageType == "PUBLIC_MESSAGE")
                 {
                     PublicMessage(dataParts[1], stream, client);
                 }
-                else if (dataType == "PRIVATE_MESSAGE")
+                else if (messageType == "PRIVATE_MESSAGE")
                 {
                     PrivateMessage(dataParts[1], stream, client);
                 }
-                else if (dataType == "LOGOUT")
+                else if (messageType == "LOGOUT")
                 {
                     RemoveClient(dataParts[1], stream, client);
                 }
@@ -108,62 +98,56 @@ class Server
         }
     }
 
-    //Hanterar registrering och sparar denna i MongoDB.
-    static void Register(string registrationData, NetworkStream stream)
+    private static void Register(string registrationData, NetworkStream stream)
     {
-        //Delar upp registreringsdatan i delar baserat på kommatecken.
-        //Det urpsrungliga formatet ser ut så här $"{regUsername},{regPassword}".
-        //Så arrayn får 2 platser, username på plats [0] och password på [1].
         string[] data = registrationData.Split(',');
-        string reply = "";
-
-        var maxUserId = collection.AsQueryable()
-            .OrderByDescending(u => u.UserId)
-            .Select(u => u.UserId)
-            .FirstOrDefault();
+        string username = data[0];
+        string password = data[1];
 
         var user = new User
         {
-            UserId = maxUserId + 1,
-            Username = data[0],
-            Password = data[1],
+            Username = username,
+            Password = password,
             Message = new List<string>()
         };
 
         collection.InsertOne(user);
-        Console.WriteLine($"User {data[0]} registered.");
-        reply = ($"User {data[0]} registered.");
+        Console.WriteLine($"User {username} registered.");
+        string reply = ($"User {username} registered.");
         SendMessageToClient(stream, reply);
     }
 
-    static void LogIn(string loginData, NetworkStream stream, TcpClient client)
+    private static void LogIn(string loginData, NetworkStream stream, TcpClient client)
     {
         string[] data = loginData.Split(',');
         string username = data[0];
         string password = data[1];
-        string reply = "";
+        string reply;
+
         var user = collection.Find(u => u.Username == username && u.Password == password)
-            .Project(u => new { u.Username, u.Password, u.Message }) //Specify which properties that should be retrived
+            .Project(u => new { u.Username, u.Password, u.Message })
             .FirstOrDefault();
 
         if (user != null)
         {
             connectedUsers.Add(client, username);
-            reply = ($"\nWelcome, {user.Username}!\n\nMessage history:\n");
-            Console.WriteLine($"{username} has logged in.");
-            SendMessageToClient(stream, reply);
+            Console.WriteLine($"{username} has logged in!");
 
             int messagesToPrint = Math.Min(30, user.Message.Count);
             int counter = 1;
+            reply = $"\nWelcome, {user.Username}!\n\nMessage history:\n";
+
             for (int i = user.Message.Count - messagesToPrint; i < user.Message.Count; i++)
             {
-                SendMessageToClient(stream, $"\n{counter}. {user.Message[i]}");
+                reply += $"{counter}. {user.Message[i]}\n";
                 counter++;
             }
 
+            SendMessageToClient(stream, reply);
+
             foreach (var otherClient in connectedUsers.Where(x => x.Key != client))
             {
-                SendMessageToClient(otherClient.Key.GetStream(), $"{username} has logged in");
+                SendMessageToClient(otherClient.Key.GetStream(), $"{username} has logged in!");
             }
         }
         else
@@ -174,29 +158,28 @@ class Server
         }
     }
 
-    static void SendMessageToClient(NetworkStream stream, string data)
+    private static void SendMessageToClient(NetworkStream stream, string data)
     {
         byte[] buffer = Encoding.ASCII.GetBytes(data);
         stream.Write(buffer, 0, buffer.Length);
     }
 
-    static void RemoveClient(string username, NetworkStream stream, TcpClient client)
+    private static void RemoveClient(string username, NetworkStream stream, TcpClient client)
     {
         if (connectedUsers.ContainsValue(username))
         {
-            // Remove the client using the username
             var clientToRemove = connectedUsers.FirstOrDefault(x => x.Value == username).Key;
             connectedUsers.Remove(clientToRemove);
             Console.WriteLine($"{username} has logged out.");
 
             foreach (var otherClient in connectedUsers.Where(x => x.Key != client))
             {
-                SendMessageToClient(otherClient.Key.GetStream(), $"{username} has logged out");
+                SendMessageToClient(otherClient.Key.GetStream(), $"{username} has logged out!");
             }
         }
     }
 
-    static void PublicMessage(string messageData, NetworkStream stream, TcpClient client)
+    private static void PublicMessage(string messageData, NetworkStream stream, TcpClient client)
     {
         string[] data = messageData.Split(',');
         string username = data[0];
@@ -204,23 +187,26 @@ class Server
 
         SavePublicMessage(username, message);
 
-        Console.WriteLine("Message saved successfully.");
-
         foreach (var otherClient in connectedUsers.Where(x => x.Key != client))
         {
             SendMessageToClient(otherClient.Key.GetStream(), $"{username}: {message}");
         }
     }
 
-    static void SavePublicMessage(string senderUsername, string message)
+    private static void SavePublicMessage(string senderUsername, string message)
     {
-        var filter = Builders<User>.Filter.Empty;
-        var update = Builders<User>.Update.Push(u => u.Message, $"{senderUsername} (Public): {message}");
-        collection.UpdateMany(filter, update);
-        Console.WriteLine("Message saved successfully to all users.");
+        foreach (var userEntry in connectedUsers)
+        {
+            var userName = userEntry.Value;
+            var filter = Builders<User>.Filter.Eq(u => u.Username, userName);
+            var update = Builders<User>.Update.Push(u => u.Message, $"{senderUsername} (Public): {message}");
+            collection.UpdateOne(filter, update);
+        }
+
+        Console.WriteLine("Message saved successfully to all connected users.");
     }
 
-    static void PrivateMessage(string messageData, NetworkStream stream, TcpClient client)
+    private static void PrivateMessage(string messageData, NetworkStream stream, TcpClient client)
     {
         string[] data = messageData.Split(',');
         string username = data[0];
@@ -255,4 +241,3 @@ class Server
         }
     }
 }
-
